0c7d834ba8cbfbd8005c534243a49e29ca11f8c4
Kevin Zhao <kevinzhao45@outlook.com>
2026-03-17 11:26:50 +0000

    added definitions for operator convexity

---
+++ LeanLiebConcavity/Defs.lean
@@ -0,0 +1,20 @@
+import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Order
+import Mathlib.Analysis.Convex.Function
+
+namespace CFC
+
+/-- `f : ℝ → ℝ` is *operator convex* on `I : Set ℝ` if, for every unital C⋆-algebra `B`
+with a compatible partial order, `cfc f` is convex on the set of self-adjoint elements of
+`B` whose spectrum is contained in `I`. -/
+def OperatorConvexOn (I : Set ℝ) (f : ℝ → ℝ) : Prop :=
+  ∀ {B : Type*} [CStarAlgebra B] [PartialOrder B] [StarOrderedRing B],
+    ConvexOn ℝ {a : B | IsSelfAdjoint a ∧ spectrum ℝ a ⊆ I} (cfc f)
+
+/-- `f : ℝ → ℝ` is *operator concave* on `I : Set ℝ` if, for every unital C⋆-algebra `B`
+with a compatible partial order, `cfc f` is concave on the set of self-adjoint elements of
+`B` whose spectrum is contained in `I`. -/
+def OperatorConcaveOn (I : Set ℝ) (f : ℝ → ℝ) : Prop :=
+  ∀ {B : Type*} [CStarAlgebra B] [PartialOrder B] [StarOrderedRing B],
+    ConcaveOn ℝ {a : B | IsSelfAdjoint a ∧ spectrum ℝ a ⊆ I} (cfc f)
+
+end CFC
