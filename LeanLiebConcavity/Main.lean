20f8c632dfa6ac063e9c8173b3ffe04dd9ccf738
Kevin Zhao <kevinzhao45@outlook.com>
2026-03-18 17:01:09 +0000

    ongoing proof for 'PerspectiveJointConvex'

--- LeanLiebConcavity/Main.lean
+++ LeanLiebConcavity/Main.lean
@@ -1,9 +1,58 @@
 import LeanLiebConcavity.Jensen
+import LeanLiebConcavity.ForMathlib
 
 /-- now need properties of Hermitian matrices -/
 example : 1 = 1 := rfl
 
-variable {A : Type*} [CStarAlgebra A] [PartialOrder A] [StarOrderedRing A]
-variable (f g : ℝ → ℝ)
+noncomputable section
 
-theorem
+open NNReal
+
+namespace CFC
+open Set
+
+--namespace IsSelfAdjoint
+variable {A : Type*} [CStarAlgebra A] [PartialOrder A] [StarOrderedRing A]
+variable {L₁ L₂ R₁ R₂ : A}
+variable {f g : ℝ → ℝ}
+
+theorem PerspectiveJointConvex
+    (hf : ContinuousOn f (Ici 0) ∧ f 0 ≤ 0)
+    (hg : ContinuousOn g (Ici 0) ∧ ∀ ⦃x : ℝ⦄, 0 < x → 0 < g x)
+    (h_opconvex : OperatorConvexOn (Ici 0) f)
+    (hg_opconcav : OperatorConcaveOn (Ici 0) g)
+    (hL : 0 ≤ L₁ ∧ 0 ≤ L₂)
+    (hR₁ : IsStrictlyPositive R₁) (hR₂ : IsStrictlyPositive R₂) :
+    ∀⦃a b : ℝ⦄, 0 ≤ a → 0 ≤ b → a + b = 1 →
+      (GenPerspective A f g) (a • L₁ + b • L₂, a • R₁ + b • R₂)
+      ≤ a • (GenPerspective A f g) (L₁, R₁) + b • (GenPerspective A f g) (L₁, R₁) := by
+    intros a b ha hb hab
+    simp only [GenPerspective]
+    let R := a • R₁ + b • R₂
+    let T₁ := (a • cfc g R₁) ^ (1/2 : ℝ) * (cfc g R) ^ (-1/2 : ℝ)
+    let T₂ := (a • cfc g R₂) ^ (1/2 : ℝ) * (cfc g R) ^ (-1/2 : ℝ)
+    have hT₁_star : star T₁ = (cfc g R) ^ (-1/2 : ℝ) * (a • cfc g R₁) ^ (1/2 : ℝ) := by
+      grind [IsSelfAdjoint.of_nonneg, IsSelfAdjoint.star_mul_eq]
+    have hT₂_star : star T₂ = (cfc g R) ^ (-1/2 : ℝ) * (a • cfc g R₂) ^ (1/2 : ℝ) := by
+      grind [IsSelfAdjoint.of_nonneg, IsSelfAdjoint.star_mul_eq]
+    have hR : IsStrictlyPositive R := by grind [isStrictlyPositive_convex_combination]
+    have hgR : IsStrictlyPositive (cfc g R) :=
+      cfc_isStrictlyPositive_of_pos hR hg.2
+      (hg.1.mono (fun x hx => spectrum_nonneg_of_nonneg hR.nonneg hx))
+    have : a • cfc g R₁ + b • cfc g R₂ ≤ cfc g R := sorry
+    have hT : star T₁ * T₁ + star T₂ * T₂ ≤ 1 := by
+      sorry -- grind [ring, add halves 1, IsSelfAdjoint.conjugate_le_conjugate (this), IsSelfAdjoint.of_nonneg, ]
+
+variable (r : ℝ)
+example : 0 ≤ L₁ ^ r := by simp
+example : IsSelfAdjoint (cfc f L₁) := by simp
+example : IsSelfAdjoint (L₁ ^ r) := IsSelfAdjoint.of_nonneg (by simp)
+example : (1 / 2: ℝ) + 1 / 2 = (1 : ℝ) := add_halves 1
+example {a b c d : A} : a * b * d + a * c * d= a * (b + c) * d := by grind only
+example {a : A} (ha : IsUnit a) (ha' : 0 ≤ a := by cfc_tac) : a ^ (1 : ℝ) * a ^ (-1 : ℝ) = 1 := by
+  grind [rpow_neg_mul_rpow (-1) ha ha']
+
+theorem PerspectiveJointConcave : 1 + 1 = 2 := by rfl
+
+
+end CFC
